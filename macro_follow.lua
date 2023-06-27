7dd7f412017beda22abea718b7e01427d9213b00
LuanGP <95983292+LuanGP@users.noreply.github.com>
2023-06-27 14:12:46 -0300

    Create macro_follow.lua

---
+++ macro_follow.lua
@@ -0,0 +1,23 @@
+setDefaultTab("Tools")
+local toFollow = storage.toFollow and storage.toFollow or ""
+local toFollowPos = {}
+UI.TextEdit(storage.toFollow or toFollow, function(widget, newText)
+  storage.toFollow = newText
+end)
+local followMacro = macro(20, "follow "..storage.toFollow, function()
+  local target = getCreatureByName(storage.toFollow)
+  if target then
+    local tpos = target:getPosition()
+    toFollowPos[tpos.z] = tpos
+  end
+  local p = toFollowPos[posz()]
+  if not p then return end
+  if autoWalk(p, 20, {ignoreNonPathable=true, precision=1}) then
+    delay(15)
+  end
+end)
+onCreaturePositionChange(function(creature, oldPos, newPos)
+  if creature:getName() == storage.toFollow and newPos then
+    toFollowPos[newPos.z] = newPos
+  end
+end)
